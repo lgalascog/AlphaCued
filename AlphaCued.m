1e293127f6d9d8bb8a63179f781aca10789a69e4
lgalascog <45874356+lgalascog@users.noreply.github.com>
2018-12-14 12:38:55 +0100

    Add files via upload

---
+++ AlphaCued.m
@@ -0,0 +1,111 @@
+clear
+close all
+addpath('./Functions');
+
+name  ='test';
+
+INFO.name              = name;
+INFO.logfilename       = ['Logfiles' filesep name '_Logfile.mat'];
+INFO.P = get_parameters;
+
+% Octave's new plotting backend 'fltk' interferes with Screen(),
+% due to internal use of OpenGL. Problem is it changes the
+% bound OpenGL rendering context behind our back and we
+% don't protect ourselves against this yet. Switch plotting backend
+% to good'ol gnuplot to work around this issue until we fix it properly
+% inside Screen():
+if IsOctave && exist('graphics_toolkit')
+    graphics_toolkit ('gnuplot');
+end
+
+% Set the screen
+Screen('Preference', 'SkipSyncTests', INFO.P.setup.skipsync); %Skip or not synchronization test
+Screen('Resolution', INFO.P.screen.screen_num, INFO.P.screen.width, ... %Set parameters of the screen
+    INFO.P.screen.height, INFO.P.screen.rate);
+
+% Create the background
+gray = GrayIndex(INFO.P.screen.screen_num);
+[myWindow, windowRect] = PsychImaging('OpenWindow', INFO.P.screen.screen_num, gray);%Open the screen and set the grey color / There ise a problem but I don't know how to use INFO.P.stim.background_color
+
+% Put square fixation
+Screen('FillRect', myWindow, INFO.P.stim.fixation_square_color, INFO.P.stim.fixation_square_position); %Add square fixation
+Screen('Flip', myWindow);
+
+WaitSecs('UntilTime', INFO.P.paradigm_blank)
+
+% Put the cue
+
+Alea_Var = rand
+
+if Alea_Var < 0.5
+    Screen ('FillRect', myWindow,INFO.P.stim.target_color,INFO.P.stim.target_positions_1)
+    Screen('Flip', myWindow);
+    'Left'
+else
+    Screen ('FillRect', myWindow,INFO.P.stim.target_color,INFO.P.stim.target_positions_2)
+    Screen('Flip', myWindow);
+    'right'
+end
+
+KbStrokeWait;
+WaitSecs('UntilTime', INFO.P.paradigm_precue)
+
+
+%% Create Detection Grating
+
+% Select one right position 
+nX = numel(INFO.P.angles_right);
+idx = randperm(1);
+angle_right = INFO.P.angles_right(idx(1:1))
+
+% Select one left position
+nX = numel(INFO.P.angles_left);
+idx = randperm(1);
+angle_left = INFO.P.angles_left(idx(1:1))
+
+% Put a grating on the right side
+%gabortex = CreateProceduralGabor(myWindow, INFO.P.screen.cx,... 
+    %INFO.P.screen.cy, [], [0.5 0.5 0.5 0.0]);
+gabortex = CreateProceduralGabor(myWindow, INFO.P.screen.cx+INFO.P.radius*cos(angle_right*pi/180),...
+    INFO.P.screen.cy+INFO.P.radius*sin(angle_right*pi/180), [], [0.5 0.5 0.5 0.0]);
+Screen('DrawTexture', myWindow, gabortex, [], [], INFO.P.grating_detection_tilt, [], [], [], [], kPsychDontDoRotation,...
+    [INFO.P.grating_detection_phase+180, INFO.P.grating_detection_freq, INFO.P.grating_detection_sc,...
+    INFO.P.grating_detection_contrast, INFO.P.grating_detection_aspectratio, 0, 0, 0]);
+Screen('Flip', myWindow);
+
+% Put a grating on the left side
+gabortex = CreateProceduralGabor(myWindow, INFO.P.screen.cx+INFO.P.radius*cos(angle_left*pi/180),...
+    INFO.P.screen.cy+INFO.P.radius*sin(angle_left*pi/180), [], [0.5 0.5 0.5 0.0]);
+Screen('DrawTexture', myWindow, gabortex, [], [], INFO.P.grating_detection_tilt, [], [], [], [], kPsychDontDoRotation,...
+    [INFO.P.grating_detection_phase+180, INFO.P.grating_detection_freq, INFO.P.grating_detection_sc,...
+    INFO.P.grating_detection_contrast, INFO.P.grating_detection_aspectratio, 0, 0, 0]);
+
+Screen('Flip', myWindow);
+ 
+%% Questions
+% Which question first
+Alea_Var = rand
+
+if Alea_Var < 0.5
+    Detection_gabor_left = DrawFormattedText(myWindow, INFO.P.text_detection_left, 'center', INFO.P.screen.cy-100, [255, 255, 255, 255]);
+    Screen('Flip', myWindow);
+    KbStrokeWait;
+    Detection_gabor_right = DrawFormattedText(myWindow, INFO.P.text_detection_right, 'center', INFO.P.screen.cy-100, [255, 255, 255, 255]);
+    Screen('Flip', myWindow);
+    KbStrokeWait;
+else
+    Detection_gabor_right = DrawFormattedText(myWindow, INFO.P.text_detection_right, 'center', INFO.P.screen.cy-100, [255, 255, 255, 255]);
+    Screen('Flip', myWindow);
+    KbStrokeWait;
+    Detection_gabor_left = DrawFormattedText(myWindow, INFO.P.text_detection_left, 'center', INFO.P.screen.cy-100, [255, 255, 255, 255]);
+    Screen('Flip', myWindow);
+    KbStrokeWait;
+end
+
+Tilt_gabor = DrawFormattedText(myWindow, INFO.P.text_tilt, 'center', INFO.P.screen.cy-100, [255, 255, 255, 255]);
+Screen('Flip', myWindow);
+
+KbStrokeWait;
+sca;
+
+
---
+++ get_parameters.m
@@ -0,0 +1,122 @@
+function [P] = get_parameters;
+
+%% ------------------------------------------------------------------------
+% Set the computer-specific parameters.
+%  ------------------------------------------------------------------------
+computername = getenv('COMPUTERNAME');
+switch computername
+    case 'LAURIE'
+        
+        thescreen = max(Screen('Screens'));
+        myres = Screen('Resolution', thescreen);
+        
+        P.screen.screen_num   = thescreen;%max(nscreens); 0 is you have only one screen (like a laptop) 1 or 2 if you have multiple screens one is usually the matlab screen
+        P.screen.width        = myres.width;
+        P.screen.height       = myres.height;
+        P.screen.rate         = myres.hz;
+        P.screen.size         = [28.8 16.2]; %screen size in centimeters.
+        P.screen.viewdist     = 55; % distance between subject and monitor
+
+    case 'BUSCH02'
+        
+        thescreen = max(Screen('Screens'));
+        myres = Screen('Resolution', thescreen);
+        
+        P.screen.screen_num   = thescreen;%max(nscreens); 0 is you have only one screen (like a laptop) 1 or 2 if you have multiple screens one is usually the matlab screen
+        P.screen.width        = myres.width;
+        P.screen.height       = myres.height;
+        P.screen.rate         = myres.hz;
+        P.screen.size         = [36 27]; %screen size in centimeters.
+        P.screen.viewdist     = 55; % distance between subject and monitor
+        
+end 
+
+%% ------------------------------------------------------------------------
+%  Parameters of the screen.
+%  Calculate size of a pixel in visual angles.
+% ------------------------------------------------------------------------
+P.screen.cx = round(P.screen.width/2); % x coordinate of screen center
+P.screen.cy = round(P.screen.height/2); % y coordinate of screen center
+
+%% -----------------------------------------------------------------------
+% Parameters of the display and stimuli
+% ------------------------------------------------------------------------
+
+P.stim.background_color = [128 128 128];
+
+P.stim.fixation_square_color = [255 255 255]; 
+P.stim.fixation_square_size = [0 0 10 10]; %should I use the program VisAng and put all dimensions in degree ?
+P.stim.fixation_square_position = CenterRectOnPointd(P.stim.fixation_square_size, P.screen.cx, P.screen.cy);
+
+P.stim.target_color = [255 255 255];
+P.stim.target_size = [0 0 50 10];
+P.stim.target_positions_1 = CenterRectOnPointd(P.stim.target_size, P.screen.cx-100, P.screen.cy);
+P.stim.target_positions_2 = CenterRectOnPointd(P.stim.target_size, P.screen.cy+500, P.screen.cy);
+
+P.target_left_positions =1; %valeur test  % 5 possible positions
+P.target_right_positions =1; %valeur test  % 5 possible positions
+
+P.text_detection_right = 'Did you see a grating on the left side of the screen';
+P.text_detection_left = 'Did you see a grating on the right side of the screen';
+P.text_tilt = 'The grating was clockwise or counterclockwise ?';
+ 
+
+
+% Gratings detection task
+P.grating_detection_res = 1*[323 323];
+P.grating_detection_phase = 0;
+P.grating_detection_sc = 40.0;
+P.grating_detection_freq = .03;
+P.grating_detection_tilt = 0;
+P.grating_detection_contrast = 100.0;
+P.grating_detection_aspectratio = 1.0;
+
+% Gratings tilt discrimination task
+P.grating_tilt_res = 1*[323 323];
+P.grating_tilt_phase = 0;
+P.grating_tilt_sc = 50.0;
+P.grating_tilt_freq = .1;
+P.grating_tilt_tilt = 0;
+P.grating_tilt_contrast = 100.0;
+P.grating_tilt_aspectratio = 1.0;
+
+% Gratings positions
+P.radius = 10
+P.angles_right = [20 -20 -40 -60 -80]
+P.angles_left = [160 200 220 240 260]
+
+% P.grating_cx_R_1 = P.screen.cx+200*cos(0.1)
+% P.grating_cy_R_1 = P.screen.cy+200*sin(0.1)
+% P.grating_positions_R_1 = CenterRectOnPointd(P.grating_detection_res, P.screen.cx+200*cos(20), P.screen.cy+200*sin(20));
+% P.grating_positions_R_2 = CenterRectOnPointd(P.grating_detection_res, P.screen.cx+200*cos(-20), P.screen.cy+200*sin(-20));
+% P.grating_positions_R_3 = CenterRectOnPointd(P.grating_detection_res, P.screen.cx+200*cos(-40), P.screen.cy+200*sin(-40));
+% P.grating_positions_R_4 = CenterRectOnPointd(P.grating_detection_res, P.screen.cx+200*cos(-60), P.screen.cy+200*sin(-60));
+% P.grating_positions_R_5 = CenterRectOnPointd(P.grating_detection_res, P.screen.cx+200*cos(-80), P.screen.cy+200*sin(-80));
+
+%% -----------------------------------------------------------------------
+% Parameters of the procedure & timing
+%  -----------------------------------------------------------------------
+
+P.paradigm_blank = 500;
+P.paradigm_precue = 100;
+P.paradigm_delay = 1500;
+P.paradigm_detection = 100;
+P.paradigm_tilt = 20;
+P.paradigm_responscue = 500;
+P.paradigm_delay = 500;
+P.paradigm_response = 1 %valeur test  %waitforbuttonpress;
+P.paradigm_ITI = 2000;
+
+
+
+%% ------------------------------------------------------------------------
+%  Define relevant buttons
+%  ------------------------------------------------------------------------
+KbName('UnifyKeyNames');
+P.keys.lkey = KbName('LeftArrow');
+P.keys.rkey = KbName('RightArrow');
+P.keys.yes = KbName('Y');
+P.keys.no = KbName('N');
+
+
+
