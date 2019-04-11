77bcc8cd36f8fce83d8977fcdc05c98f91755df3
lgalascog <45874356+lgalascog@users.noreply.github.com>
2019-04-11 18:34:58 +0200

    Add files via upload

--- Functions/one_trial_trial1.m
+++ Functions/one_trial_trial1.m
@@ -1,57 +1,26 @@
-function [INFO, isQuit] = one_trial_AlphaCued(myWindow, INFO, itrial)
-
-
-isQuit=0;
-% Run one trial
-
-% Recalibration
-if INFO.P.setup.isEYEtrack && itrial~=1; %
-    EyelinkRecalibration(P);
-    Eyelink('Message', 'SYNCTIME');
-end
-
-if INFO.P.setup.isEYEtrack
-    Eyelink('Message', 'TrialID %d', itrial);
-end
-
-EyeHasMoved = 0;
-EyeIsLost = 0;
-% put last trial at end of block and note that something was
-% wrong
-if INFO.P.setup.isEYEtrack
-    if EyeHasMoved == 1 || EyeIsLost == 1
-        INFO.T.GazeHasMovedOrEyeIsLost = 1;
-        EyeHasMoved = 0;
-        EyeIsLost = 0;
-    end
-end
+%One trial train
+function [INFO, isQuit] = one_trial_exemple(myWindow, INFO, itrial)
 
 % --------------------------------------------------------
-% Prestimulus interval
+% Instructions cue
 % --------------------------------------------------------
+
+instructions = DrawFormattedText(myWindow, INFO.P.text_instruction_cue,...
+    'center', INFO.P.screen.cy-500, [255, 255, 255, 255], [],[],[], 2);
 my_optimal_fixationpoint(myWindow, INFO.P.screen.cx, INFO.P.screen.cy,... 
    INFO.P.stim.fixation_size, INFO.P.stim.fixation_square_color,...
    INFO.P.stim.background_color, INFO.P.screen.pixperdeg);
 Screen('Flip', myWindow);
-WaitSecs(INFO.P.paradigm_blank);
 
+KbWait()
 
-% --------------------------------------------------------
-% Precue
-% --------------------------------------------------------
-set_pre_cue(myWindow,INFO, itrial);
-WaitSecs(INFO.P.paradigm_precue);
+Screen ('FillRect', myWindow, INFO.P.stim.cue_color, INFO.P.stim.cue_rects_2);
+my_optimal_fixationpoint(myWindow, INFO.P.screen.cx, INFO.P.screen.cy,...
+    INFO.P.stim.fixation_size, INFO.P.stim.fixation_square_color,...
+    INFO.P.stim.background_color, INFO.P.screen.pixperdeg)
+Screen('Flip', myWindow);
 
-% send trigger to EEG
-if INFO.P.setup.isEYEtrack
-    EyeLinkMessage = 'Cue';
-    Eyelink('Message', EyeLinkMessage, itrial);
-    Trigger  = 10 + INFO.T(itrial).pre_cue;
-    SendTrigger(Trigger, P.TriggerDuration);
-    % check if gaze position is within boudaries
-    [~,~,EyeHasMoved] = EyelinkGetGaze(P,1);
-end
-
+WaitSecs(INFO.P.paradigm_precue_train1);
 % --------------------------------------------------------
 % Delay
 % --------------------------------------------------------
@@ -64,46 +33,37 @@ WaitSecs(INFO.P.paradigm_delay_between_cue_and_stim);
 % --------------------------------------------------------
 % probe target
 % --------------------------------------------------------
-set_probe_target(myWindow,INFO,itrial);
-WaitSecs(INFO.P.paradigm_detection);
 
-my_optimal_fixationpoint(myWindow, INFO.P.screen.cx, INFO.P.screen.cy,...
+instructions = DrawFormattedText(myWindow, INFO.P.text_instruction_circle,...
+    'center', INFO.P.screen.cy-500, [255, 255, 255, 255], [],[],[], 2);
+my_optimal_fixationpoint(myWindow, INFO.P.screen.cx, INFO.P.screen.cy,... 
    INFO.P.stim.fixation_size, INFO.P.stim.fixation_square_color,...
    INFO.P.stim.background_color, INFO.P.screen.pixperdeg);
 Screen('Flip', myWindow);
-%WaitSecs(INFO.P.paradigm_delay2)
 
-if INFO.T(itrial).probes == 1
-    Trigger2 = 22;
-    Trigger3 = 31;
-end
-if INFO.T(itrial).probes == 2
-    Trigger2 = 21;
-    Trigger3 = 32;
-end
-if INFO.T(itrial).probes == 3
-    Trigger2 = 21;
-    Trigger3 = 31;
-end
-if INFO.T(itrial).probes == 4
-    Trigger2 = 22;
-    Trigger3 = 32;
-end
+KbWait([], 2);
 
-% send trigger to EEG
-if INFO.P.setup.isEYEtrack
-    EyeLinkMessage = 'Probes ';
-    Eyelink('Message', EyeLinkMessage, itrial);
-    SendTrigger(Trigger2, P.TriggerDuration);
-    SendTrigger(Trigger3, P.TriggerDuration);
-end
+set_probe_target(myWindow,INFO, itrial) ;
+
+WaitSecs(INFO.P.paradigm_detection_train1);
+
+
+
 % --------------------------------------------------------
 % attention target
 % --------------------------------------------------------
+instructions = DrawFormattedText(myWindow, INFO.P.text_instruction_gabor,...
+    'center', INFO.P.screen.cy-500, [255, 255, 255, 255], [],[],[], 2);
+my_optimal_fixationpoint(myWindow, INFO.P.screen.cx, INFO.P.screen.cy,... 
+   INFO.P.stim.fixation_size, INFO.P.stim.fixation_square_color,...
+   INFO.P.stim.background_color, INFO.P.screen.pixperdeg);
+Screen('Flip', myWindow);
+KbWait([], 3);
+
 gabortex = CreateProceduralGabor(myWindow, INFO.P.grating_tilt_width_pix,...
     INFO.P.grating_tilt_height_pix, [], [0.5 0.5 0.5 0.0]);
 set_attention_target(myWindow,gabortex,INFO,itrial);
-WaitSecs(INFO.P.paradigm_tilt_train1);
+WaitSecs(INFO.P.paradigm_precue_train1);
 
 % % --------------------------------------------------------
 % % response cue + Delay
@@ -171,5 +131,4 @@ if EyeIsLost ==1
     EyelinkRecalibration(P);
     Eyelink('Message', 'SYNCTIME');
 end
-
 
