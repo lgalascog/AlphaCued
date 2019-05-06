dbae5eed75b2497280fa3073f0b8f29647d9fd2d
lgalascog <45874356+lgalascog@users.noreply.github.com>
2019-05-06 10:28:11 +0200

    Delete DrawScreen_with_fixation_square.m

--- DrawScreen_with_fixation_square.m
+++
@@ -1,6 +0,0 @@
-function [INFO] = DrawScreen_with_fixation_square(myWindow,INFO)
-
-% Display square fixation
-Screen('FillRect', myWindow, INFO.P.stim.fixation_square_color,...
-    INFO.P.stim.fixation_rects); %Add square fixation
-Screen('Flip', myWindow);
