feb3b9f96442315946fc5efb2016225c229f3fb0
lgalascog <45874356+lgalascog@users.noreply.github.com>
2019-05-23 16:20:33 +0200

    Delete Instructions.m

--- Functions/Instructions.m
+++
@@ -1,8 +0,0 @@
-function [INFO] = instruction (INFO)
-
-
-
-
-instructions = DrawFormattedText(myWindow, INFO.P.text_detection_left,...
-    'center', INFO.P.screen.cy-100, [255, 255, 255, 255]);
-Screen('Flip', myWindow);
